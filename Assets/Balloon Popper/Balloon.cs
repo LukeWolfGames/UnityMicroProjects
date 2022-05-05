cf76d8969d94455c97600f4c345097132ad4304f
Luke Wolf <lukewolfgames@gmail.com>
2022-05-05 22:23:23 +1000

    Add gitignore and made first changes to Balloon script

---
+++ Assets/Balloon Popper/Balloon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    public int scoreToGive = 1;
+    public int 
+
+    OnMouseDown() 
+    {
+
+    }
+
+}
